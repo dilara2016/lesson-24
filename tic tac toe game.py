6691b794947088ea36a958dbf73ea9910c40281f
dilara2016 <m.123.hunain@gmail.com>
2024-12-27 16:44:28 +0100

    Update tic tac toe game.py

--- tic tac toe game.py
+++ tic tac toe game.py
@@ -1,23 +1,23 @@
-theBoard =  {'7': '' ,'8': '' ,'9': '' ,'4': '' ,'5': '' ,'6': '' , '1': '' , '2': '' , '3': '' }
-board_keys = []
-for key in theBoard:
-   board_keys.append(key) 
-   def printBoard(board):
-       print(board['7'] + '|' + board['8'] + '|' + board['9'])
+thehomework =  {'7': '' ,'8': '' ,'9': '' ,'4': '' ,'5': '' ,'6': '' , '1': '' , '2': '' , '3': '' }
+homework_keys = []
+for key in thehomework:
+   homework_keys.append(key) 
+   def printBoard(homework):
+       print(homework['7'] + '|' + homework['8'] + '|' + homework['9'])
        print('-+-+-')
-       print(board['4'] + '|' + board['5'] + '|' + board['6'])
+       print(homework['4'] + '|' + homework['5'] + '|' + homework['6'])
        print('-+-+-')
-       print(board['1'] + '|' + board['2'] + '|' + board['3'])
+       print(homework['1'] + '|' + hommework['2'] + '|' + homework['3'])
 def game():
 
     turn = 'x'
     count = 0
     for i in range(10):
-     printBoard[theBoard]
+     print homework[thehomework]
      print("its youre turn," + turn + ".move to wich place?")
      move = input()
-     if theBoard[move] =='':
-        theBoard[move] = turn
+     if thehomework[move] =='':
+        thehomework[move] = turn
         count+=1
         else:
 print("that place is already filled")
