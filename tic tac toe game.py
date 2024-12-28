98945c944a3626d76bcdc02a384ab5a04892dd84
dilara2016 <m.123.hunain@gmail.com>
2024-12-28 10:08:18 +0100

    Update tic tac toe game.py

--- tic tac toe game.py
+++ tic tac toe game.py
@@ -2,7 +2,7 @@ thehomework =  {'7': '' ,'8': '' ,'9': '' ,'4': '' ,'5': '' ,'6': '' , '1': '' ,
 homework_keys = []
 for key in thehomework:
    homework_keys.append(key) 
-   def printBoard(homework):
+   def printhomework(homework):
        print(homework['7'] + '|' + homework['8'] + '|' + homework['9'])
        print('-+-+-')
        print(homework['4'] + '|' + homework['5'] + '|' + homework['6'])
